243efc5532531fa88fb736015db1dcc1977e179c
Vignesh Gadhari <124498042+VigneshGadhari@users.noreply.github.com>
2025-02-11 20:10:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Serve static files from root directory
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
